38ff0a5b3104a810548b826404ed5ff65a49adf8
Guilherme Laranjeira <guilr14@gmail.com>
2022-12-03 16:47:30 -0300

    Adicionar randomizador de palavras

---
+++ ProjetoFinal/Program.cs
@@ -0,0 +1,33 @@
+﻿bool vitoria, derrota, fim;
+int vidas = 6;
+
+string palavra = GerarPalavra().ToUpper();
+
+char[] letrasCertas = palavra.ToUpper().ToCharArray();
+
+char[] letrasTabuleiro = new char[palavra.Length];
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Método para gerar palavra aleatória de uma array
+string GerarPalavra()
+{
+    Random rd = new Random();
+
+    string[] palavras = {"Monitor", "Vizinho", "Gramado", "Toalha", "Terceiro", "Faringe", "Assustar", "Mordomo", "Rodovia", "Eternidade", "Capela",
+        "Fluxo","Sardinha", "Pirata", "Ferro", "Difícil", "Trancar", "Casamento", "Cativeiro", "Descriçao", "Maremoto", "Coletivo", "Grafite", "Civilizaçao",
+        "Vitoria", "Exemplo", "Adivinhar", "Xampu", "Personagem", "Traidor", "Cítrico", "Especular", "Afrouxar", "Laranjeira", "Composto", "Progenitor","Granizo", "Dispositivo",
+        "Armadilha", "Poesia", "Umbilical", "Relacionamento", "Capacete", "Envelope", "Cronometragem",  "Calculadora", "Hipnotizar", "Marinheiro", "Perpetuo"};
+
+    return palavras[rd.Next(palavras.Length)];
+}
