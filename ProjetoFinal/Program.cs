c5ce3d9b74dcf683d061e187c821b02a82a1b471
Guilherme Laranjeira <guilr14@gmail.com>
2022-12-03 17:18:56 -0300

    Adicionar tabuleiro e lógica do jogo

--- ProjetoFinal/Program.cs
+++ ProjetoFinal/Program.cs
@@ -1,21 +1,84 @@
-﻿bool vitoria, derrota, fim;
+﻿bool vitoria = false, derrota = false, fim = false;
 int vidas = 6;
 
 string palavra = GerarPalavra().ToUpper();
 
 char[] letrasCertas = palavra.ToUpper().ToCharArray();
 
+
 char[] letrasTabuleiro = new char[palavra.Length];
+ConstruirArrayDoTabuleiro(letrasTabuleiro);
 
+char[] jogadas = new char[27];
 
+//Se fosse lista não precisaria desse iterador;
+int iterador = 0;
 
+do
+{
+    ConstruirTabuleiro(vidas, letrasTabuleiro, jogadas);
 
+    char jogada = Jogada();
 
+    if (LetraValida(jogada))
+    {
+        if (!jogadas.Contains(jogada))
+        {
+            jogadas[iterador] = jogada;
+            iterador++;
+            bool acerto = false;
 
+            for(int i = 0; i < letrasCertas.Length; i++)
+            {
+                if (letrasCertas[i] == jogada)
+                {
+                    letrasTabuleiro[i] = jogada;
+                    acerto = true;
+                }
+            }
+            if (!acerto)
+            {
+                vidas--;
+            }
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine($"\nA letra '{jogada}' já foi selecionada! Tente de novo\n");
+            continue;
+        }
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine($"\n'{jogada}' não é uma letra do alfabeto. Por favor, insira uma letra do alfabeto válida!\n");
+        continue;
+    }
 
+    Console.Clear();
 
+    //Verifica se o jogo acabou
+    if(vidas < 1)
+    {
+        derrota = true;
+        fim = true;
+    }
+    else if (!letrasTabuleiro.Contains('_'))
+    {
+        vitoria = true;
+        fim = true;
+    }
 
+} while (!fim);
 
+if (vitoria)
+{
+    Console.WriteLine($"Parabéns, você venceu! A palavra era '{palavra}'. Sobraram {vidas} vidas");
+}
+else if (derrota)
+{
+    Console.WriteLine($"Infelizmente você perdeu :(. A palavra correta era: '{palavra}'");
+}
 
 
 
@@ -30,4 +93,63 @@ string GerarPalavra()
         "Armadilha", "Poesia", "Umbilical", "Relacionamento", "Capacete", "Envelope", "Cronometragem",  "Calculadora", "Hipnotizar", "Marinheiro", "Perpetuo"};
 
     return palavras[rd.Next(palavras.Length)];
+}
+
+char[] ConstruirArrayDoTabuleiro(char[] letrasTabuleiro)
+{
+    for (int i = 0; i < letrasTabuleiro.Length; i++)
+    {
+        letrasTabuleiro[i] = '_';
+    }
+    return letrasTabuleiro; 
+}
+
+void ConstruirTabuleiro(int vida, char[] letraTabuleiro, char[] jogadas)
+{
+    Console.WriteLine(" *_____*");
+    Console.WriteLine($" |/    '         Vidas: {vidas}");
+    Console.WriteLine(" |");
+    Console.WriteLine(" |");
+    Console.WriteLine(" |");
+    Console.WriteLine(" |");
+    Console.Write(" |");
+
+    foreach (var a in letrasTabuleiro)
+    {
+        Console.Write(a + " ");
+    }
+
+    Console.Write("\nLetras escolhidas =  ");
+    foreach (var b in jogadas)
+    {
+        Console.Write(b + " ");
+    }
+}
+
+char Jogada()
+{
+    bool convert;
+    do
+    {
+        Console.WriteLine("\n\n\nDigite uma letra:");
+        string jogadaString = Console.ReadLine().ToUpper();
+        convert = char.TryParse(jogadaString, out char jogada);
+        return jogada;
+    } while (!convert);
+}
+
+bool LetraValida(char jogada)
+{
+    //todas as letras do alfabeto
+    char[] alfabeto = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R','S','T','U','V','W', 'X', 'Y', 'Z', };
+
+    if (alfabeto.Contains(jogada))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
