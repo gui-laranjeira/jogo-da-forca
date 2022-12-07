b02a261919d00ddb2495a6e5362557222e2150a9
Guilherme Laranjeira <guilr14@gmail.com>
2022-12-07 13:08:08 -0300

    inclusao classe BaseJogo e Validacao

--- ProjetoFinal/Program.cs
+++ ProjetoFinal/Program.cs
@@ -4,27 +4,27 @@ bool vitoria = false, derrota = false, fim = false;
 int vidas = 6;
 string mensagem="";
 
-string palavra = GerarPalavra().ToUpper();
+string palavra = BaseJogo.GerarPalavra().ToUpper();
 
 char[] letrasCertas = palavra.ToUpper().ToCharArray();
 
 
 char[] letrasTabuleiro = new char[palavra.Length];
-ConstruirArrayDoTabuleiro(letrasTabuleiro);
+BaseJogo.ConstruirArrayDoTabuleiro(letrasTabuleiro);
 
 char[] jogadas = new char[27];
 
 //Se fosse lista não precisaria desse iterador;
 int iterador = 0;
-string tema = Tema(palavra);
+string tema = BaseJogo.Tema(palavra);
 
 do
 {
     Tabuleiro.ConstruirTabuleiro(vidas, letrasTabuleiro, jogadas, tema, mensagem);
 
-    char jogada = Jogada();
+    char jogada = BaseJogo.Jogada();
 
-    if (LetraValida(jogada))
+    if (Validacao.LetraValida(jogada))
     {
         if (!jogadas.Contains(jogada))
         {
@@ -93,117 +93,6 @@ else if (derrota)
 
 
 
-//Método para gerar palavra aleatória de uma array
-string GerarPalavra()
-{
-    Random rd = new Random();
-
-    string[] palavras = { "AMARELO","BRANCO", "AZUL","VIOLETA","CACHORRO","PASSARINHO","GATO",
-                    "PEIXE","PIJAMA","CUECA","CASACO","JAQUETA","ESTOJO","CADERNO", "TESOURA","CORRETIVO","BOLO","MOUSSE","LASANHA",
-                    "PIZZA", "CANECA", "GARFO", "ESCUMADEIRA","PANELA","PEDIATRA","OFTALMOLOGISTA","CARDIOLOGISTA","NEUROLOGISTA"};
-
-    //teste de commit
-
-    return palavras[rd.Next(palavras.Length)];
-}
-
-char[] ConstruirArrayDoTabuleiro(char[] letrasTabuleiro)
-{
-    for (int i = 0; i < letrasTabuleiro.Length; i++)
-    {
-        letrasTabuleiro[i] = '_';
-    }
-    return letrasTabuleiro; 
-}
 
 
 
-char Jogada()
-{
-    bool convert;
-    do
-    {
-        Console.WriteLine("\n\n\nDigite uma letra:");
-        string jogadaString = Console.ReadLine().ToUpper();
-        convert = char.TryParse(jogadaString, out char jogada);
-        return jogada;
-    } while (!convert);
-}
-
-bool LetraValida(char jogada)
-{
-    //todas as letras do alfabeto
-    char[] alfabeto = { 'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R','S','T','U','V','W', 'X', 'Y', 'Z', };
-
-    if (alfabeto.Contains(jogada))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-
-static string Tema(string palavra)
-{
-    string tematica = " * ";
-
-    switch (palavra)
-    {
-        case "AMARELO":
-        case "BRANCO":
-        case "AZUL":
-        case "VIOLETA":
-            tematica = "COR";
-            break;
-
-        case "CACHORRO":
-        case "PASSARINHO":
-        case "GATO":
-        case "PEIXE":
-            tematica = "BICHINHO DE ESTIMAÇÃO";
-            break;
-
-        case "PIJAMA":
-        case "CUECA":
-        case "CASACO":
-        case "JAQUETA":
-            tematica = "VESTIMENTA";
-            break;
-
-        case "ESTOJO":
-        case "CADERNO":
-        case "TESOURO":
-        case "CORRETIVO":
-            tematica = "MATERIAL ESCOLAR";
-            break;
-
-        case "BOLO":
-        case "MOUSSE":
-        case "LASANHA":
-        case "PIZZA":
-            tematica = "COMIDA";
-            break;
-
-        case "CANECA":
-        case "GARFO":
-        case "ESCUMADEIRA":
-        case "PANELA":
-            tematica = "UTENSILIOS DE COZINHA";
-            break;
-
-        case "PEDIATRA":
-        case "OFTALMOLOGISTA":
-        case "CARDIOLOGISTA":
-        case "NEUROLOGISTA":
-            tematica = "ESPECIALIDADE MEDICA";
-            break;
-
-
-    }
-    return tematica;
-}
